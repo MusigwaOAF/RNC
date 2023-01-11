e26d94067ad3b777675641761c8142e01e2223bf
Musigwa Pacifique <68857067+Musigwa@users.noreply.github.com>
2023-01-11 21:04:44 +0200

    Update the color generator function

--- src/utils/index.js
+++ src/utils/index.js
@@ -26,36 +26,72 @@ const calcTint = (value, index = .0001) => Math.round(value + (255 - value) * in
 
 const calcShade = (value, index = .4) => Math.round(value * index);
 
-export const genColor = (type = 'solo', format = 'hex', opacity = 1) => {
-  // type can be oneOf(['solo' || 'tint' || 'shade' || 'soloTint' || 'soloShade' || 'tintShade' || 'all'])
-  // format can oneOf(['hex', 'rgba'])
-  // opacity can be oneOf([number, array[numbers]])
+/**
+ * Helps generate a usable format's random color
+ * @author Musigwa Pacifique
+ * @param {string} [format='hex']
+ * @param {string} [type='solo']
+ * @param {number} [opacity=1]
+ * @returns {string | string[]}
+ * Returns either a string representation of the generated color or
+ * an array of strings: Generated color, and its corresponding
+ * tint and shade colors depending on the passed arguments
+ * @example 1. genColor(); ==> '#68f538'
+ * @example 2. genColor({ format: 'rgba' }); ==> 'rgba(159, 112, 193, 1)'
+ * @example 3. genColor({ format: 'rgba', opacity: 0.8 }); ==> 'rgba(159, 112, 193, 0.8)'
+ * @example 4. genColor({ format: 'rgba', opacity: 0.8, type: 'solo' }); ==> 'rgba(156, 121, 83, 0.5)'
+ * @example 5. genColor({ format: 'rgba', opacity: 0.8, type: 'shade' }); ==> 'rgba(70, 92, 30, 0.8)'
+ * @example 6. genColor({ format: 'rgba', opacity: 0.8, type: 'tint' }); ==> 'rgba(70, 92, 30, 0.8)'
+ * @example 7. genColor({ format: 'rgba', opacity: 0.8, type: 'soloTint' }); ==> ["rgba(68, 211, 54, 0.8)", "rgba(68, 211, 54, 0.8)"]
+ * @example 8. genColor({ format: 'rgba', opacity: 0.8, type: 'soloShade' }); ==> ["rgba(80, 226, 166, 0.8)", "rgba(32, 90, 66, 0.8)"]
+ * @example 9. genColor({ format: 'rgba', opacity: 0.8, type: 'all' }); ==> ["rgba(115, 93, 80, 0.8)", "rgba(171, 158, 150, 0.8)", "rgba(46, 37, 32, 0.8)"]
+ */
+export const genColor = (args?: ColorGenArgs) => {
+  const { format = 'hex', type = 'solo', opacity = 1 } = args ?? {};
+  const genNumber = (upper = 256) => Math.floor(Math.random() * upper);
+  const calcTint = (value, index = 0.4) => Math.round(value + (255 - value) * index);
+  const calcShade = (value, index = 0.4) => Math.round(value * index);
+
   const red = genNumber(); // 0 - 255
   const green = genNumber(); // 0 - 255
   const blue = genNumber(); // 0 - 255
-
-  const formatFn = (array) => format === 'rgba' ? toRGBA(array, opacity) : toHEX(array);
 
   const solo = [red, green, blue];
   const tint = [calcTint(red), calcTint(green), calcTint(blue)];
   const shade = [calcShade(red), calcShade(green), calcShade(blue)];
 
+  // Find out the color's contrast.
+  const yiq = (red * 299 + green * 587 + blue * 114) / 1000;
+  const contentColor = yiq >= 128 ? 'black' : 'white';
+
+  const toHEX = ([red = 255, green = 255, blue = 255]: number[]) =>
+    `#${((red << 16) + (green << 8) + blue).toString(16).padStart(6, '0')}`;
+  const toRGBA = ([red = 255, green = 255, blue = 255]: number[], opacity: number = 1) =>
+    `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+
+  const formatFn = (array: number[]) => (format === 'rgba' ? toRGBA(array, opacity) : toHEX(array));
+
+  const color = formatFn(solo);
+  const tintColor = formatFn(tint);
+  const shadeColor = formatFn(shade);
+
   switch (type) {
     case 'solo':
-      return formatFn(solo);
+      return color;
     case 'tint':
-      return formatFn(tint);
+      return tintColor;
     case 'shade':
-      return formatFn(shade);
+      return shadeColor;
     case 'soloTint':
-      return [formatFn(solo), formatFn(tint)];
+      return [color, tintColor];
     case 'soloShade':
-      return [formatFn(solo), formatFn(shade)];
+      return [color, shadeColor];
     case 'tintShade':
-      return [formatFn(tint), formatFn(shade)];
+      return [tintColor, shadeColor];
     case 'all':
-      return [formatFn(solo), formatFn(tint), formatFn(shade)];
+      return [color, tintColor, shadeColor];
     default:
-      return formatFn(solo);
+      return color;
   }
 };
+
