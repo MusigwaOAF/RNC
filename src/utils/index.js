7e315c23e0613fd915af45d48faef068fa94dc2f
Musigwa Pacifique <68857067+Musigwa@users.noreply.github.com>
2023-01-11 21:56:08 +0200

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,41 +16,40 @@ export const numberDelimiter = number => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
-const genNumber = (upper = 256) => Math.floor(Math.random() * upper);
-
-const toHEX = ([red = 255, green = 255, blue = 255]) => `#${((red << 16) + (green << 8) + blue).toString(16).padStart(6, '0')}`;
-
-const toRGBA = ([red = 255, green = 255, blue = 255], opacity = 1) => `rgba(${red}, ${green}, ${blue}, ${opacity})`;
-
-const calcTint = (value, index = .0001) => Math.round(value + (255 - value) * index);
-
-const calcShade = (value, index = .4) => Math.round(value * index);
-
 /**
  * Helps generate a usable format's random color
  * @author Musigwa Pacifique
  * @param {string} [format='hex']
  * @param {string} [type='solo']
  * @param {number} [opacity=1]
- * @returns {string | string[]}
- * Returns either a string representation of the generated color or
- * an array of strings: Generated color, and its corresponding
- * tint and shade colors depending on the passed arguments
+ * @returns Either an object representation of the generated color and its matching onColor or
+ * an array of objects representation: Generated color, its corresponding
+ * tint and shade colors as well as the matching contentColor => 'onColor' for each depending on the passed type value.
  * @example 1. genColor(); ==> '#68f538'
  * @example 2. genColor({ format: 'rgba' }); ==> 'rgba(159, 112, 193, 1)'
  * @example 3. genColor({ format: 'rgba', opacity: 0.8 }); ==> 'rgba(159, 112, 193, 0.8)'
  * @example 4. genColor({ format: 'rgba', opacity: 0.8, type: 'solo' }); ==> 'rgba(156, 121, 83, 0.5)'
  * @example 5. genColor({ format: 'rgba', opacity: 0.8, type: 'shade' }); ==> 'rgba(70, 92, 30, 0.8)'
  * @example 6. genColor({ format: 'rgba', opacity: 0.8, type: 'tint' }); ==> 'rgba(70, 92, 30, 0.8)'
- * @example 7. genColor({ format: 'rgba', opacity: 0.8, type: 'soloTint' }); ==> ["rgba(68, 211, 54, 0.8)", "rgba(68, 211, 54, 0.8)"]
+ * @example 7. genColor({ format: 'rgba', opacity: 0.8, type: 'soloTint' }); ==> ["rgba(90, 205, 38, 0.8)", "rgba(156, 225, 125, 0.8)"]
  * @example 8. genColor({ format: 'rgba', opacity: 0.8, type: 'soloShade' }); ==> ["rgba(80, 226, 166, 0.8)", "rgba(32, 90, 66, 0.8)"]
  * @example 9. genColor({ format: 'rgba', opacity: 0.8, type: 'all' }); ==> ["rgba(115, 93, 80, 0.8)", "rgba(171, 158, 150, 0.8)", "rgba(46, 37, 32, 0.8)"]
  */
-export const genColor = (args?: ColorGenArgs) => {
+export const genColor = (args?: ColorGenArgs): ColorGenReturnType | ColorGenReturnType[] => {
   const { format = 'hex', type = 'solo', opacity = 1 } = args ?? {};
   const genNumber = (upper = 256) => Math.floor(Math.random() * upper);
   const calcTint = (value, index = 0.4) => Math.round(value + (255 - value) * index);
   const calcShade = (value, index = 0.4) => Math.round(value * index);
+
+  const toHEX = ([red = 255, green = 255, blue = 255]: number[]) =>
+    `#${((red << 16) + (green << 8) + blue).toString(16).padStart(6, '0')}`;
+  const toRGBA = ([red = 255, green = 255, blue = 255]: number[], opacity: number = 1) =>
+    `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+
+  const genOnColor = ([red = 255, green = 255, blue = 255]: number[]) => {
+    const yiq = (red * 299 + green * 587 + blue * 114) / 1000;
+    return yiq >= 128 ? 'black' : 'white';
+  };
 
   const red = genNumber(); // 0 - 255
   const green = genNumber(); // 0 - 255
@@ -60,20 +59,11 @@ export const genColor = (args?: ColorGenArgs) => {
   const tint = [calcTint(red), calcTint(green), calcTint(blue)];
   const shade = [calcShade(red), calcShade(green), calcShade(blue)];
 
-  // Find out the color's contrast.
-  const yiq = (red * 299 + green * 587 + blue * 114) / 1000;
-  const contentColor = yiq >= 128 ? 'black' : 'white';
-
-  const toHEX = ([red = 255, green = 255, blue = 255]: number[]) =>
-    `#${((red << 16) + (green << 8) + blue).toString(16).padStart(6, '0')}`;
-  const toRGBA = ([red = 255, green = 255, blue = 255]: number[], opacity: number = 1) =>
-    `rgba(${red}, ${green}, ${blue}, ${opacity})`;
-
   const formatFn = (array: number[]) => (format === 'rgba' ? toRGBA(array, opacity) : toHEX(array));
 
-  const color = formatFn(solo);
-  const tintColor = formatFn(tint);
-  const shadeColor = formatFn(shade);
+  const color = { color: formatFn(solo), onColor: genOnColor(solo) };
+  const tintColor = { color: formatFn(tint), onColor: genOnColor(tint) };
+  const shadeColor = { color: formatFn(shade), onColor: genOnColor(shade) };
 
   switch (type) {
     case 'solo':
@@ -88,7 +78,7 @@ export const genColor = (args?: ColorGenArgs) => {
       return [color, shadeColor];
     case 'tintShade':
       return [tintColor, shadeColor];
-    case 'all':
+    case 'soloTintShade':
       return [color, tintColor, shadeColor];
     default:
       return color;
